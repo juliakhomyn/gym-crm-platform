ec8a6acbb1fea3f0d10df9ecaddcc03dd1d8cb18
juliakhomyn <jkhomyn1381@gmail.com>
2026-06-22 11:45:07 +0200

    GIA-244 Implement circuit breaker - explicitly specify the HTTP methods fallback endpoints accept

--- gateway-service/src/main/java/com/gym/crm/gateway/controller/FallbackController.java
+++ gateway-service/src/main/java/com/gym/crm/gateway/controller/FallbackController.java
@@ -4,17 +4,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.PATCH;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+
 @RestController
 @RequestMapping("/fallback")
 public class FallbackController {
     private static final String SERVICE_UNAVAILABLE_MESSAGE = "%s Service is currently unavailable. Please try again later.";
 
-    @RequestMapping("/core")
+    @RequestMapping(path = "/core", method = {GET, POST, PUT, PATCH, DELETE})
     public ResponseEntity<String> gymCoreFallback() {
         return ResponseEntity.ok(String.format(SERVICE_UNAVAILABLE_MESSAGE, "Gym Core"));
     }
 
-    @RequestMapping("/workload")
+    @RequestMapping(path = "/workload", method = {GET, POST, PUT, PATCH, DELETE})
     public ResponseEntity<String> workloadFallback() {
         return ResponseEntity.ok(String.format(SERVICE_UNAVAILABLE_MESSAGE, "Workload"));
     }
