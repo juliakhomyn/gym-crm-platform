2929ce5778654b14240e3073347783d11e4e522f
juliakhomyn <jkhomyn1381@gmail.com>
2026-06-27 13:33:47 +0200

    GIA-289 Migrate workload updates to ActiveMQ - mock TrainerWorkloadMessageSender in AuthorizationIntegrationTest

--- gym-core-service/src/test/java/com/gym/crm/core/facade/AuthorizationIntegrationTest.java
+++ gym-core-service/src/test/java/com/gym/crm/core/facade/AuthorizationIntegrationTest.java
@@ -11,12 +11,14 @@ import com.gia.openapi.model.TrainerGetResponse;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.junit5.api.DBRider;
+import com.gym.crm.core.messaging.TrainerWorkloadMessageSender;
 import com.gym.crm.core.search.filter.TraineeTrainingFilter;
 import com.gym.crm.core.search.filter.TrainerTrainingFilter;
 import com.gym.crm.core.utils.TestDataProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
@@ -40,6 +42,9 @@ class AuthorizationIntegrationTest {
     private static final String TRAINEE_USERNAME = "Simone.Radcliffe";
     private static final String TRAINER_USERNAME = "Owen.Castleberry";
     private static final String NOT_AUTHORIZED_USERNAME = "Nora.Pemberton";
+
+    @MockBean
+    private TrainerWorkloadMessageSender sender;
 
     @Autowired
     private GymFacade gymFacade;
